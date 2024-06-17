b850740c472c9209585415222d31c8f55dc11a3c
sheryl acree <sacree@tql.com>
2024-06-17 14:32:07 -0400

    Add project files.

---
+++ Banking App/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_App;
+public class Account {
+    public int AccountId { get; set; } = 0;
+    public string Description { get; set; } = string.Empty;
+    public decimal Balance { get; set; } = 0;
+
+    public bool Deposit(decimal Amount) {
+        if (Amount <= 0) return false;
+        Console.WriteLine("Amount cannot be zero or negative!");
+        Balance += Amount;
+        return true;
+    }
+
+    public bool Withdraw(decimal Amount) {
+        if (Amount <= 0) {
+            Console.WriteLine("Amount cannot be zero or negative!");
+            return false;
+        }
+
+        if (Amount > Balance) {
+            Console.WriteLine("Insufficient funds!");
+            return false;
+        }
+            Balance -= Amount;
+            return true;
+        }
+  
+    
+    public bool Transfer(decimal Amount, Account account) {
+        var success = Withdraw(Amount);
+        if (success == true) { 
+            account.Deposit(Amount);
+        }
+        return true;
+    }
+}
---
+++ Banking App/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+
+namespace Banking_App;
+
+//Create a Console Project called Banking App
+//Create properties for :
+//AccountId integer
+// Description string - like Checking, Savings, Joint, etc
+//Balance decimal
+
+//Create methods for :
+//Depositing into the Balance property-This method will have parameters-The value cannot be negative
+//Withdrawing from the Balance property-This method has required parameters. The value cannot be negative. The value cannot exceed Balance. 
+//Display the Current Balance-This method does not have any parameters. It returns the value of the Balance as decimal.  
+//When run, the prompt should show the user that they can list the function they want followed by a parameter (if needed)
+
+//Examples: $ deposit 100 $deposit 40 $ withdraw 30 $ withdraw 120 $ balance (responds with the current value of the Balance property
+
+internal class Program {
+    static void Main(string[] args) {
+
+     Account a1 = new Account();
+     Account a2 = new Account();
+
+    Console.WriteLine($"Balance is {a1.Balance:C}");
+    a1.Deposit(500);
+    Console.WriteLine($"Balance is {a1.Balance:C}");
+    a1.Withdraw(230);
+     Console.WriteLine($"Balance is {a1.Balance:C}");
+    a1.Withdraw(300);
+    Console.WriteLine($"Balance is {a1.Balance:C}");
+    a1.Deposit(-200);
+        a1.Transfer(100, a2);
+
+
+
+    }
+}
